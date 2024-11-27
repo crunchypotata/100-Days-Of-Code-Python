aebcc94985c6a51a1df6cfe8305b411010359280
Aleksandra Iakimova <yakimova.cg@gmail.com>
2024-11-27 19:01:40 +0100

    Website scrapping

---
+++ Day096_Scrapping_Website.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://news.ycombinator.com/" 
+
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+myLinks = soup.find_all("span", {"class": "titleline"})
+# print(len(myLinks))
+
+things = ["replit", "python", "apple", "microsoft"]
+
+for link in myLinks:
+    text = link.text
+    textList = text.split()
+    containsWord = False
+    for word in textList:
+        if word.lower() in things:
+            containsWord = True
+    if containsWord:
+        print(link.text)
+
+        myLink = link.find_all("a")
+        print(myLink[0]["href"])
+    
